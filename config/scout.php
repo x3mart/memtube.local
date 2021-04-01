7274aaa99dad0d24e073048166ad428fb6d3fb74
x3mart <x3mat@gmail.com>
2021-04-01 20:55:01 +0300

    scout queue & tnt asYouType on

--- config/scout.php
+++ config/scout.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'queue' => env('SCOUT_QUEUE', false),
+    'queue' => env('SCOUT_QUEUE', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -123,7 +123,7 @@ return [
             'max_expansions' => 50,
             'distance' => 2
         ],
-        'asYouType' => false,
+        'asYouType' => true,
         'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
         'maxDocs' => env('TNTSEARCH_MAX_DOCS', 500),
     ],
